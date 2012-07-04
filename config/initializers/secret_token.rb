e9251d4e4440eb34a20c66c2fc1451eb273d5e3b
Nick Malcolm <nick@southgatelabs.com>
2012-07-04 18:31:17 +1200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Statisfy::Application.config.secret_token = 'c6e1fd5941c081f860fa509cab2efd1f7bdaa3fc899b29e78c01c0c5ddaa7ae0d637a6cf514fd240b73bb53d869f5be9173404909a703af9e411fee2b32e8ab4'

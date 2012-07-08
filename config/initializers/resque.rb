fe40c554030d8c89c6051df850256d7563b41171
Nick Malcolm <nick@southgatelabs.com>
2012-07-08 18:48:35 +1200

    Use redistogo for redis backend on production

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,5 +1,11 @@
 require 'resque'
 require 'resque/server'
+
+if Rails.env.production?
+  uri = URI.parse ENV['REDISTOGO_URL']
+  Resque.redis = Redis.new :host => uri.host, :port => uri.port, :password => uri.password
+end
+
 Resque::Server.use Rack::Auth::Basic do |username, password|
   username == "statisfy.me"
   password == "YcUp2&)h^8j]67H"

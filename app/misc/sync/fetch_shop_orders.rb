6a3f4e12ae36c0ac32899602e42dc3566ee3607d
Nick Malcolm <nick@southgatelabs.com>
2012-07-09 00:08:04 +1200

    Makes order fetcher stick within limits, use pagination, and fetch 250 orders at a time

--- app/misc/sync/fetch_shop_orders.rb
+++ app/misc/sync/fetch_shop_orders.rb
@@ -1,19 +1,35 @@
 module Sync
   class FetchShopOrders
+    SHOPIFY_PAGE_LIMIT = 250
     @queue = :sync
-  
-    def self.perform(shop_id, page = 1)
+    
+    def self.perform(shop_id)
       raise ArgumentError, "Shop #{shop_id} does not exist" unless (shop = Shop.find_by_id(shop_id))
     
       shop.shopify_session
       
       count = ShopifyAPI::Order.count
-      puts "Shop #{shop_id} has #{count} Orders"
+      credit_left = ShopifyAPI.credit_left # Keep track of credit locally to save on API calls
+      
       if count > 0
-        order_details = []
-        page += count.divmod(250).first
-        while page > 0
-          orders = ShopifyAPI::Order.find(:all, :params => {:limit => 250, :page => page})
+        # The number of pages required to get all Orders
+        pages = (count/"#{SHOPIFY_PAGE_LIMIT}".to_d).ceil
+        #Start on the last page
+        page = pages 
+        # The last page we can do with this many API calls left
+        last_page = [0, pages-credit_left].max 
+        
+        # Go from the last page and do as many as we can
+        while page > last_page
+          
+          orders = ShopifyAPI::Order.find(
+              :all, 
+              params: {
+                limit: SHOPIFY_PAGE_LIMIT, 
+                page: page,
+                fields: "id,shipping_address"
+              })
+          
           orders.each do |shopify_order|
           
             Order.create do |order|
@@ -22,6 +38,7 @@ module Sync
             end
           
           end
+          
           page -= 1
         end
       end
--- test/unit/sync/fetch_shop_orders_test.rb
+++ test/unit/sync/fetch_shop_orders_test.rb
@@ -1,10 +1,85 @@
 require 'test_helper'
 class FetchShopOrdersTest < ActiveSupport::TestCase
   
+  setup do
+    @shop_id = Factory(:shop, access_token: "ABC").id
+  end
+  
   test "perform raises ArgumentError if invalid shop id is given" do
     assert_raises ArgumentError do
       Sync::FetchShopOrders.perform(-1)
     end
   end
-
+  
+  test "can fetch 250 orders with 1 credit_left" do
+    ShopifyAPI::Order.expects(:count).once.returns(250) # 250 Orders to fetch
+    ShopifyAPI.expects(:credit_left).once.returns(1) # 1 API call left
+    
+    ShopifyAPI::Order.expects(:find).once.with(
+      :all,
+      params: {
+        limit: 250,
+        page: 1,
+        fields: "id,shipping_address"
+      }
+    ).returns([])
+    
+    Sync::FetchShopOrders.perform(@shop_id)
+  end
+  
+  test "can't fetch any orders with 0 credit_left" do
+    ShopifyAPI::Order.expects(:count).once.returns(250) # 250 Orders to fetch
+    ShopifyAPI.expects(:credit_left).once.returns(0) # 0 API call left
+    
+    ShopifyAPI::Order.expects(:find).never
+    
+    Sync::FetchShopOrders.perform(@shop_id)
+  end
+  
+  test "can fetch 500 orders with 2 credit_left" do
+    ShopifyAPI::Order.expects(:count).once.returns(500)
+    ShopifyAPI.expects(:credit_left).once.returns(2)
+    
+    ShopifyAPI::Order.expects(:find).twice.returns([])
+    
+    Sync::FetchShopOrders.perform(@shop_id)
+  end
+  
+  test "can fetch oldest 250/500 orders with 1 credit_left" do
+    ShopifyAPI::Order.expects(:count).once.returns(500)
+    ShopifyAPI.expects(:credit_left).once.returns(1)
+    
+    ShopifyAPI::Order.expects(:find).once.with(
+      :all,
+      params: {
+        limit: 250,
+        page: 2,
+        fields: "id,shipping_address"
+      }
+    ).returns([])
+    
+    Sync::FetchShopOrders.perform(@shop_id)
+  end
+  
+  test "fetch creates orders returned by Shopify" do
+    ShopifyAPI::Order.expects(:count).once.returns(1)
+    ShopifyAPI.expects(:credit_left).once.returns(500)
+    
+    mock_order = stub()
+    mock_order.expects(:id).once.returns(1234)
+    mock_shipping_address = stub()
+    mock_shipping_address.expects(:country_code).once.returns("NZ")
+    mock_order.expects(:shipping_address).once.returns(mock_shipping_address)
+    
+    ShopifyAPI::Order.expects(:find).returns([mock_order])
+    
+    assert_difference "Order.count" do
+      Sync::FetchShopOrders.perform(@shop_id)
+    end
+    
+    order = Order.last
+    assert_equal 1234, order.shopify_id
+    assert_equal "NZ", order.shipping_country_code
+  end
+  
 end

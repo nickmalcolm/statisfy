917a5da42f1262f66368377bc369d79968d8014f
Nick Malcolm <nick@southgatelabs.com>
2012-07-12 20:51:10 +1200

    Show ranking on the home page

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -13,13 +13,21 @@ class HomeControllerTest < ActionController::TestCase
     usa = Factory(:country, code: "US", name: "United States")
     nz  = Factory(:country, code: "NZ", name: "New Zealand")
     crown1 = Factory(:crown, country: usa, shop: shop2)
-    crown2 = Factory(:crown, country: nz,  lost_at: 5.minutes.ago)
+    crown2 = Factory(:crown, country: nz,  shop: shop2, lost_at: 5.minutes.ago)
     crown3 = Factory(:crown, country: nz,  shop: shop1)
     
     get :index
     
-    assert_select "li", class: "us", text: "Walmart rules over United States"
-    assert_select "li", class: "nz", text: "$2 Shop rules over New Zealand"
+    assert_select "li", class: "us" do
+      assert_select "h3", "United States"
+      assert_select "li#crown_#{crown1.id}", "Walmart reigns."
+    end
+    assert_select "li", class: "nz" do
+      assert_select "h3", "New Zealand"
+      assert_select "li#crown_#{crown3.id}", "$2 Shop reigns."
+      assert_select "li#crown_#{crown2.id}", "Walmart was usurped 5 minutes ago."
+    end
+    
   end
 
 end

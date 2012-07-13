840bc2a0e769a9d3e5a5c00226c6d91d1d08f6ee
Nick Malcolm <nick@southgatelabs.com>
2012-07-13 21:40:58 +1200

    Sync orders before awarding crowns

--- app/misc/award_crowns.rb
+++ app/misc/award_crowns.rb
@@ -2,6 +2,9 @@ class AwardCrowns
   @queue = :award
   
   def self.perform
+    Shop.all.each do |s|
+      Resque.enqueue(Sync::FetchShopOrders, s.id)
+    end
     Country.all.each do |c|
       Resque.enqueue(AwardCountryCrown, c.id)
     end
--- test/unit/misc/award_crowns_test.rb
+++ test/unit/misc/award_crowns_test.rb
@@ -2,6 +2,24 @@ require 'test_helper'
 
 class AwardTest < ActiveSupport::TestCase
   
+  test "perform calls every shop" do
+    (shop1 = mock()).expects(:id).once
+    (shop2 = mock()).expects(:id).once
+    Shop.expects(:all).returns([shop1, shop2])
+    AwardCrowns.perform()
+  end
+  
+  test "perform enqueues FetchShopOrders job" do
+    (shop1 = mock()).stubs(:id).returns(1)
+    (shop2 = mock()).stubs(:id).returns(3)
+    Shop.stubs(:all).returns([shop1, shop2])
+    
+    Resque.expects(:enqueue).once.with(Sync::FetchShopOrders, 1)
+    Resque.expects(:enqueue).once.with(Sync::FetchShopOrders, 3)
+    
+    AwardCrowns.perform()
+  end
+  
   test "perform calls on every country" do
     (country1 = mock()).expects(:id).once
     (country2 = mock()).expects(:id).once
@@ -9,7 +27,7 @@ class AwardTest < ActiveSupport::TestCase
     AwardCrowns.perform()
   end
   
-  test "perform enqueues award country crown job" do
+  test "perform enqueues AwardCountryCrown job" do
     (country1 = mock()).stubs(:id).returns(1)
     (country2 = mock()).stubs(:id).returns(3)
     Country.stubs(:all).returns([country1, country2])
